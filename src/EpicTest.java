17727372153bb4cc599ab505a861b79f4a1fff90
genesis177 <alinagdl1@yandex.ru>
2025-03-07 03:28:25 +0500

    Revert "Sprint 6 solution"

--- src/EpicTest.java
+++ src/EpicTest.java
@@ -1,46 +1,42 @@
-package ru.yandex.practicum.javadeveloper.javakanban;
+package EpicTest1;
 
-
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class EpicTest {
+class EpicTest {
     private Epic epic;
 
     @BeforeEach
-    public void setUp() {
+    void setUp() {
         epic = new Epic("Epic Title", "Epic Description");
     }
 
     @Test
-    public void testAddSubtask() {
+    void testAddSubtask() {
         epic.addSubtask(1);
         List<Integer> subtaskIds = epic.getSubtaskIds();
-
         assertEquals(1, subtaskIds.size());
         assertEquals(1, subtaskIds.get(0).intValue());
     }
 
     @Test
-    public void testClearSubtasks() {
+    void testClearSubtasks() {
         epic.addSubtask(1);
         epic.addSubtask(2);
         epic.clearSubtasks();
-
-        assertTrue(epic.getSubtaskIds().isEmpty());
+        Assertions.assertTrue(epic.getSubtaskIds().isEmpty());
     }
 
     @Test
-    public void testGetSubtaskIds() {
+    void testGetSubtaskIds() {
         epic.addSubtask(1);
         epic.addSubtask(2);
         List<Integer> subtaskIds = epic.getSubtaskIds();
-
         assertEquals(2, subtaskIds.size());
         assertTrue(subtaskIds.contains(1));
         assertTrue(subtaskIds.contains(2));
